609078624e6c533fb58d0eafce4c7fb42bd75495
KongMD <KongMDSteam@gmail.com>
2019-10-05 09:44:08 -0400

    Add new ThingDef 'QE_Building_OrganVat' Create new Organ Vat that uses Bills instead of Gizmos for growing

---
+++ QEE/Things/Building_GrowerBase_WorkTable.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace QEthics
+{
+    /// <summary>
+    /// Base class for grower buildings.
+    /// </summary>
+    public abstract class Building_GrowerBase_WorkTable : Building_WorkTable, IThingHolder
+    {
+        private GrowerProperties growerPropsInt = null;
+
+        /// <summary>
+        /// Grower building properties.
+        /// </summary>
+        public GrowerProperties GrowerProps
+        {
+            get
+            {
+                if (growerPropsInt == null)
+                {
+                    growerPropsInt = def.GetModExtension<GrowerProperties>();
+
+                    //Fallback; Is defaults.
+                    if (growerPropsInt == null)
+                    {
+                        growerPropsInt = new GrowerProperties();
+                    }
+                }
+
+                return growerPropsInt;
+            }
+        }
+
+        public CompPowerTrader PowerTrader
+        {
+            get
+            {
+                return GetComp<CompPowerTrader>();
+            }
+        }
+
+        /// <summary>
+        /// Ticks needed until the crafting is finished.
+        /// </summary>
+        public abstract int TicksNeededToCraft { get; }
+
+        /// <summary>
+        /// Current progress being made during crafting.
+        /// </summary>
+        public int craftingProgress;
+
+        public int TicksLeftToCraft
+        {
+            get
+            {
+                return TicksNeededToCraft - craftingProgress;
+            }
+        }
+
+        public float CraftingProgressPercent
+        {
+            get
+            {
+                if (TicksNeededToCraft == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    float percentComplete = (float)craftingProgress / (float)TicksNeededToCraft;
+                    return percentComplete > 1.0f ? 1.0f : percentComplete;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Status of this crafter.
+        /// </summary>
+        public CrafterStatus status = CrafterStatus.Idle;
+
+        /// <summary>
+        /// Internal container representation of stored items.
+        /// </summary>
+        protected ThingOwner innerContainer = null;
+
+        /// <summary>
+        /// The crafter order processor. Is set by the player during Idle status.
+        /// </summary>
+        public ThingOrderProcessor orderProcessor;
+
+        public Building_GrowerBase_WorkTable()
+        {
+            innerContainer = new ThingOwner<Thing>(this, false, LookMode.Deep);
+            orderProcessor = new ThingOrderProcessor(this);
+            billStack = new BillStack(this);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref craftingProgress, "craftingProgress");
+            Scribe_Values.Look(ref status, "status");
+            Scribe_Deep.Look(ref innerContainer, "innerContainer", this, false, LookMode.Deep);
+            Scribe_Deep.Look(ref orderProcessor, "orderProcessor", this);
+            /*if(Scribe.mode == LoadSaveMode.LoadingVars)
+            {
+                orderProcessor.Notify_ContentsChanged();
+            }*/
+        }
+
+        public void GetChildHolders(List<IThingHolder> outChildren)
+        {
+            ThingOwnerUtility.AppendThingHoldersFromThings(outChildren, GetDirectlyHeldThings());
+        }
+
+        public ThingOwner GetDirectlyHeldThings()
+        {
+            return innerContainer;
+        }
+
+        public override string GetInspectString()
+        {
+            StringBuilder builder = new StringBuilder(base.GetInspectString());
+
+            //Status
+            builder.AppendLine();
+            builder.AppendLine("QE_GrowerStatus".Translate() + ": " + TransformStatusLabel(("QE_GrowerStatus_" + status.ToString()).Translate()));
+
+            //Ingredients: Needed
+            if(status == CrafterStatus.Filling)
+            {
+                builder.AppendLine("QE_GrowerIngredientsNeeded".Translate() + ": " + orderProcessor.FormatCachedIngredientsInThingOrderProcessor());
+            }
+
+            //Crafting Progress
+            /*if (status == CrafterStatus.Crafting)
+            {
+                builder.AppendLine("QE_GrowerCraftingProgress".Translate() + ": " + CraftingProgressPercent.ToStringPercent());
+            }*/
+
+            //Ingredients: Filled
+            /*if (innerContainer.Count > 0)
+            {
+                builder.AppendLine("QE_GrowerIngredientsFilled".Translate() + ": " + innerContainer.FormatIngredientsInThingOwner());
+            }*/
+
+            return builder.ToString().TrimEndNewlines();
+        }
+
+        public virtual string TransformStatusLabel(string label)
+        {
+            return label;
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (this.IsHashIntervalTick(60))
+            {
+
+                switch (status)
+                {
+                    case CrafterStatus.Idle:
+                        {
+                            Tick_Idle();
+                        }
+                        break;
+                    case CrafterStatus.Filling:
+                        {
+                            //Check if any Things are lost in the order processor.
+                            orderProcessor.Cleanup();
+
+                            if (orderProcessor.requestsLost)
+                            {
+                                //Abort if any of the requests were lost.
+                                Reset();
+                                Notify_ThingLostInOrderProcessor();
+                                orderProcessor.requestsLost = false;
+                                Tick_Idle();
+                            }
+                            else
+                            {
+                                Tick_Filling();
+                            }
+                        }
+                        break;
+                    case CrafterStatus.Crafting:
+                        {
+                            Tick_Crafting();
+                        }
+                        break;
+                    case CrafterStatus.Finished:
+                        {
+                            Tick_Finished();
+                        }
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Idle tick.
+        /// </summary>
+        public virtual void Tick_Idle()
+        {
+            QEEMod.TryLog("All recipes: "+this.def.AllRecipes.Count);
+
+            List<RecipeDef> allDefsListForReading = DefDatabase<RecipeDef>.AllDefsListForReading;
+            for (int j = 0; j < allDefsListForReading.Count; j++)
+            {
+                if(allDefsListForReading[j].defName == "Natural_Heart" )
+                {
+
+                    if (allDefsListForReading[j].recipeUsers != null)
+                    {
+                        QEEMod.TryLog("heart recipeUsers: " + allDefsListForReading[j].recipeUsers.ToString());
+                    }
+                    else
+                        QEEMod.TryLog("heart doesn't have recipes");
+
+                    if (allDefsListForReading[j].recipeUsers != null && allDefsListForReading[j].recipeUsers.Contains(this.def))
+                    {
+                        QEEMod.TryLog("heart has recipes, but doesn't contain " + this.def.defName);
+                    }
+
+                    //else
+                    //    QEEMod.TryLog("heart recipeusers broken");
+                }
+                //if (allDefsListForReading[j].recipeUsers != null && allDefsListForReading[j].recipeUsers.Contains(this.def))
+                //{
+                //    QEEMod.TryLog("Adding " + allDefsListForReading[j].defName + " to the list");
+                //}
+            }
+        }
+
+        /// <summary>
+        /// Filling tick.
+        /// </summary>
+        public virtual void Tick_Filling()
+        {
+            if(orderProcessor.PendingRequests.Count() <= 0)
+            {
+                //Log.Message("PendingRequests is 0. Starting crafting...");
+                status = CrafterStatus.Crafting;
+                Notify_CraftingStarted();
+            }
+        }
+
+        public virtual void Notify_ThingLostInOrderProcessor()
+        {
+
+        }
+
+        public virtual void Notify_StartedCarryThing(Pawn pawn)
+        {
+
+        }
+
+        public virtual void Notify_CraftingStarted()
+        {
+
+        }
+
+        public virtual void Notify_CraftingFinished()
+        {
+
+        }
+
+        /// <summary>
+        /// Crafting tick.
+        /// </summary>
+        public virtual void Tick_Crafting()
+        {
+            //Increment crafting.
+            bool doCrafting = true;
+            if(PowerTrader != null && !PowerTrader.PowerOn)
+            {
+                doCrafting = false;
+            }
+            if(doCrafting)
+            {
+                craftingProgress = craftingProgress + 60;
+                if (craftingProgress >= TicksNeededToCraft)
+                {
+                    craftingProgress = TicksNeededToCraft;
+
+                    status = CrafterStatus.Finished;
+
+                    Notify_CraftingFinished();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finished tick.
+        /// </summary>
+        public virtual void Tick_Finished()
+        {
+            
+        }
+
+        /// <summary>
+        /// The crafting has finished.
+        /// </summary>
+        public virtual void CraftingFinished()
+        {
+            Reset();
+        }
+
+        public virtual void Reset()
+        {
+            craftingProgress = 0;
+            status = CrafterStatus.Idle;
+        }
+
+        public void FillThing(Thing thing)
+        {
+            if(thing != null)
+            {
+                innerContainer.TryAddOrTransfer(thing, true);
+                orderProcessor.Notify_ContentsChanged();
+            }
+        }
+
+        public virtual bool TryExtractProduct(Pawn actor)
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ QEE/Things/Building_OrganVat.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using Verse.AI;
+
+namespace QEthics
+{
+    /// <summary>
+    /// Building for growing things like organs. Requires constant maintenance in order to not botch the crafting. Dirty rooms increase maintenance drain even more.
+    /// </summary>
+    public class Building_OrganVat : Building_GrowerBase_WorkTable, IMaintainableGrower
+    {
+        static Building_OrganVat()
+        {
+            cleanlinessCurve.Add(-5.0f, 5.00f);
+            cleanlinessCurve.Add(-2.0f, 1.75f);
+            cleanlinessCurve.Add(0.0f, 1.0f);
+            cleanlinessCurve.Add(0.4f, 0.35f);
+            cleanlinessCurve.Add(2.0f, 0.1f);
+        }
+
+        public static SimpleCurve cleanlinessCurve = new SimpleCurve();
+
+        /// <summary>
+        /// Current active recipe being crafted.
+        /// </summary>
+        public GrowerRecipeDef activeRecipe;
+
+        //public override int TicksNeededToCraft => activeRecipe?.craftingTime ?? 0;
+        public override int TicksNeededToCraft => (int)(activeRecipe?.craftingTime * QEESettings.instance.organGrowthRateFloat ?? 0);
+
+        /// <summary>
+        /// From 0.0 to 1.0. If the maintenance is below 50% there is a chance for failure.
+        /// </summary>
+        public float scientistMaintenance;
+
+        /// <summary>
+        /// From 0.0 to 1.0. If the maintenance is below 50% there is a chance for failure.
+        /// </summary>
+        public float doctorMaintenance;
+
+        public float RoomCleanliness
+        {
+            get
+            {
+                Room room = this.GetRoom(RegionType.Set_Passable);
+                if (room != null)
+                {
+                    return room.GetStat(RoomStatDefOf.Cleanliness);
+                }
+
+                return 0f;
+            }
+        }
+
+        private VatGrowerProperties vatGrowerPropsInt;
+
+        public VatGrowerProperties VatGrowerProps
+        {
+            get
+            {
+                if (vatGrowerPropsInt == null)
+                {
+                    vatGrowerPropsInt = def.GetModExtension<VatGrowerProperties>();
+
+                    //Fallback; Is defaults.
+                    if (vatGrowerPropsInt == null)
+                    {
+                        vatGrowerPropsInt = new VatGrowerProperties();
+                    }
+                }
+
+                return vatGrowerPropsInt;
+            }
+        }
+
+        public float ScientistMaintenance { get => scientistMaintenance; set => scientistMaintenance = value; }
+
+        public float DoctorMaintenance { get => doctorMaintenance; set => doctorMaintenance = value; }
+
+        public Building_OrganVat() : base()
+        {
+            
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Defs.Look(ref activeRecipe, "activeRecipe");
+            Scribe_Values.Look(ref scientistMaintenance, "scientistMaintenance");
+            Scribe_Values.Look(ref doctorMaintenance, "doctorMaintenance");
+        }
+
+        public override string GetInspectString()
+        {
+            if(!(ParentHolder is Map))
+            {
+                return null;
+            }
+
+            StringBuilder builder = new StringBuilder(base.GetInspectString());
+
+            if (status == CrafterStatus.Crafting)
+            {
+                builder.AppendLine();
+                builder.AppendLine("QE_VatGrowerMaintenance".Translate(String.Format("{0:0%}", scientistMaintenance), 
+                    String.Format("{0:0%}", doctorMaintenance)));
+
+                builder.AppendLine("QE_VatGrowerCleanlinessMult".Translate(cleanlinessCurve.Evaluate(RoomCleanliness).ToString("0.00")));
+            }
+
+            return builder.ToString().TrimEndNewlines();
+        }
+
+        public override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            //Draw bottom graphic
+            Vector3 drawAltitude = drawLoc;
+            if(VatGrowerProps.bottomGraphic != null)
+            {
+                VatGrowerProps.bottomGraphic.Graphic.Draw(drawAltitude, Rotation, this);
+            }
+
+            //Draw product
+            drawAltitude += new Vector3(0f, 0.005f, 0f);
+            if ((status == CrafterStatus.Crafting || status == CrafterStatus.Finished) && activeRecipe != null && activeRecipe.productGraphic != null)
+            {
+                Material material = activeRecipe.productGraphic.Graphic.MatSingle;
+
+                float scale = (0.2f + (CraftingProgressPercent * 0.8f)) * VatGrowerProps.productScaleModifier;
+                Vector3 scaleVector = new Vector3(scale, 1f, scale);
+                Matrix4x4 matrix = default(Matrix4x4);
+                matrix.SetTRS(drawAltitude + VatGrowerProps.productOffset, Quaternion.AngleAxis(0f, Vector3.up), scaleVector);
+
+                Graphics.DrawMesh(MeshPool.plane10, matrix, material, 0);
+            }
+
+            //Draw top graphic
+            if (VatGrowerProps.topGraphic != null)
+            {
+                drawAltitude += new Vector3(0f, 0.005f, 0f);
+                VatGrowerProps.topGraphic.Graphic.Draw(drawAltitude, Rotation, this);
+            }
+
+            //Draw top detail graphic
+            if (VatGrowerProps.topDetailGraphic != null && (PowerTrader?.PowerOn ?? false))
+            {
+                drawAltitude += new Vector3(0f, 0.005f, 0f);
+                VatGrowerProps.topDetailGraphic.Graphic.Draw(drawAltitude, Rotation, this);
+            }
+
+            //Draw glow graphic
+            if ((status == CrafterStatus.Crafting || status == CrafterStatus.Finished) && VatGrowerProps.glowGraphic != null && (PowerTrader?.PowerOn ?? false))
+            {
+                drawAltitude += new Vector3(0f, 0.005f, 0f);
+                VatGrowerProps.glowGraphic.Graphic.Draw(drawAltitude, Rotation, this);
+            }
+        }
+
+        public override void Notify_CraftingStarted()
+        {
+            innerContainer.ClearAndDestroyContents();
+        }
+
+        public override void Notify_CraftingFinished()
+        {
+            Messages.Message("QE_MessageGrowingDone".Translate(activeRecipe.productDef.LabelCap), new LookTargets(this), MessageTypeDefOf.PositiveEvent, false);
+        }
+
+        public override void Tick_Crafting()
+        {
+            base.Tick_Crafting();
+
+            //Deduct maintenance, fail if any of them go below 0%.
+            float powerModifier = 1f;
+            if (PowerTrader != null && !PowerTrader.PowerOn)
+            {
+                powerModifier = 15f;
+            }
+            float cleanlinessModifer = cleanlinessCurve.Evaluate(RoomCleanliness);
+            float decayRate = 0.0012f * cleanlinessModifer * powerModifier / (QEESettings.instance.maintRateFloat);
+
+            scientistMaintenance -= decayRate;
+            doctorMaintenance -= decayRate;
+
+            if(scientistMaintenance < 0f || doctorMaintenance < 0f)
+            {
+                //Fail the craft, waste all products.
+                Reset();
+                if (activeRecipe?.productDef?.defName != null)
+                {
+                    Messages.Message("QE_OrgaVatMaintFailMessage".Translate(activeRecipe.productDef.defName.Named("ORGANNAME")), new LookTargets(this), MessageTypeDefOf.NegativeEvent);
+                }
+                else
+                {
+                    Messages.Message("QE_OrgaVatMaintFailFallbackMessage".Translate(), new LookTargets(this), MessageTypeDefOf.NegativeEvent);
+                }
+            }
+        }
+
+        public override bool TryExtractProduct(Pawn actor)
+        {
+            Thing product = ThingMaker.MakeThing(activeRecipe.productDef);
+            product.stackCount = activeRecipe.productAmount;
+
+            if(status == CrafterStatus.Finished)
+            {
+                CraftingFinished();
+            }
+
+            //place product on the interaction cell of the grower
+            GenPlace.TryPlaceThing(product, this.InteractionCell, this.Map, ThingPlaceMode.Near);
+
+            //Pawn extracting product hauls product back to storage
+            IntVec3 storeCell;
+            IHaulDestination haulDestination;
+            if (StoreUtility.TryFindBestBetterStorageFor(product, actor, product.Map, StoragePriority.Unstored, actor.Faction, out storeCell, out haulDestination, false))
+            {
+                if (storeCell.IsValid || haulDestination != null)
+                {
+                    actor.jobs.StartJob(HaulAIUtility.HaulToStorageJob(actor, product), JobCondition.Succeeded);
+                }
+            }
+
+            return true;
+        }
+
+        public void StartCraftingRecipe(GrowerRecipeDef recipeDef)
+        {
+            //Setup recipe order
+            orderProcessor.Reset();
+            IngredientUtility.FillOrderProcessorFromVatGrowerRecipe(orderProcessor, recipeDef);
+            orderProcessor.Notify_ContentsChanged();
+
+            //Initialize maintenance
+            scientistMaintenance = 0.25f;
+            doctorMaintenance = 0.25f;
+
+            activeRecipe = recipeDef;
+            status = CrafterStatus.Filling;
+        }
+
+        public override void Notify_ThingLostInOrderProcessor()
+        {
+            StopCrafting();
+        }
+
+        public void StopCrafting()
+        {
+            craftingProgress = 0;
+            orderProcessor.Reset();
+
+            status = CrafterStatus.Idle;
+            activeRecipe = null;
+            if(innerContainer.Count > 0)
+            {
+                innerContainer.TryDropAll(InteractionCell, Map, ThingPlaceMode.Near);
+            }
+        }
+
+        public override string TransformStatusLabel(string label)
+        {
+            string recipeLabel = activeRecipe?.LabelCap ?? "QE_VatGrowerNoRecipe".Translate();
+
+            if (status == CrafterStatus.Filling || status == CrafterStatus.Finished)
+            {
+                return label + " " + recipeLabel.CapitalizeFirst();
+            }
+            if (status == CrafterStatus.Crafting)
+            {
+                //return label + " " + recipeLabel.CapitalizeFirst() + " (" + CraftingProgressPercent.ToStringPercent() + ")";
+                float daysRemaining = GenDate.TicksToDays(TicksLeftToCraft);
+                if (daysRemaining > 1.0)
+                {
+                    return recipeLabel.CapitalizeFirst() + " (" + String.Format("{0:0.0}", daysRemaining) +
+                        " " + "QE_VatGrowerDaysRemaining".Translate() + ")";
+                }
+                else
+                {
+                    return " " + recipeLabel.CapitalizeFirst() + " (" + String.Format("{0:0.0}", (TicksLeftToCraft / 2500.0f)) +
+                        " " + "QE_VatGrowerHoursRemaining".Translate() + ")";
+                }
+            }
+
+            return base.TransformStatusLabel(label);
+        }
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            foreach (Gizmo gizmo in base.GetGizmos())
+            {
+                yield return gizmo;
+            }
+
+            if(status == CrafterStatus.Idle)
+            {
+                yield return new Command_Action()
+                {
+                    defaultLabel = "QE_VatGrowerStartCraftingGizmoLabel".Translate(),
+                    defaultDesc = "QE_VatGrowerStartCraftingGizmoDescription".Translate(),
+                    icon = ContentFinder<Texture2D>.Get("Things/Item/Health/HealthItemNatural", true),
+                    order = -100,
+                    action = delegate ()
+                    {
+                        List<FloatMenuOption> options = new List<FloatMenuOption>();
+
+                        foreach (GrowerRecipeDef recipeDef in DefDatabase<GrowerRecipeDef>.AllDefs.OrderBy(def => def.orderID))
+                        {
+                            bool disabled = false;
+                            if (recipeDef.requiredResearch != null && !recipeDef.requiredResearch.IsFinished)
+                            {
+                                disabled = true;
+                            }
+
+                            string label = null;
+                            if (disabled)
+                            {
+                                label = "QE_VatGrowerStartCraftingFloatMenuDisabled".Translate(recipeDef.LabelCap, recipeDef.requiredResearch.LabelCap);
+                            }
+                            else
+                            {
+                                label = recipeDef.LabelCap;
+                            }
+
+                            FloatMenuOption option = new FloatMenuOption(label, delegate ()
+                            {
+                                StartCraftingRecipe(recipeDef);
+                            });
+
+                            option.Disabled = disabled;
+
+                            options.Add(option);
+                        }
+
+                        if (options.Count > 0)
+                        {
+                            Find.WindowStack.Add(new FloatMenu(options));
+                        }
+                    }
+                };
+            }
+            else
+            {
+                if (status != CrafterStatus.Finished)
+                {
+                    yield return new Command_Action()
+                    {
+                        defaultLabel = "QE_VatGrowerStopCraftingGizmoLabel".Translate(),
+                        defaultDesc = "QE_VatGrowerStopCraftingGizmoDescription".Translate(),
+                        icon = ContentFinder<Texture2D>.Get("UI/Designators/Cancel", true),
+                        order = -100,
+                        action = delegate ()
+                        {
+                            StopCrafting();
+                        }
+                    };
+                    if (Prefs.DevMode)
+                    {
+                        yield return new Command_Action()
+                        {
+                            defaultLabel = "QE_VatGrowerDebugFinishGrowing".Translate(),
+                            defaultDesc = "QE_OrganVatDebugFinishGrowingDescription".Translate(),
+                            //icon = ContentFinder<Texture2D>.Get("UI/Designators/Cancel", true),
+                            action = delegate ()
+                            {
+                                craftingProgress = TicksNeededToCraft;
+                            }
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
